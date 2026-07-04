c2f83f072771f5a964b44329c39e36ab85e09bc9
Andrei-360-tour <156935416+Andrei-360-tour@users.noreply.github.com>
2026-07-04 15:01:53 +0300

    Commit

---
+++ data.js
@@ -0,0 +1,87 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-overview1",
+      "name": "OVERVIEW1",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": -2.7560141626746955,
+        "pitch": 0.08037789007346952,
+        "fov": 1.3412181875493232
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.8019913986531755,
+          "pitch": 0.08184134530453413,
+          "rotation": 0,
+          "target": "1-overview2"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-overview2",
+      "name": "OVERVIEW2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 3.132464125003846,
+        "pitch": -0.023322929143478888,
+        "fov": 1.3412181875493232
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.3470058941998486,
+          "pitch": -0.001296971538181424,
+          "rotation": 0,
+          "target": "0-overview1"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "SH-4_04.07.2026",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
